15a51a6e1d89f5e47dfade38af5b7d8bf772344e
Elliott Lockwood <lockwood721@gmail.com>
2020-08-14 19:44:23 -0500

    build folder add

---
+++ build/precache-manifest.4f5d5fbd918956a1eacc8782ebc5b754.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25af0c039e5cd73b8e5c3203bd0f6c12",
+    "url": "/User-Directory/index.html"
+  },
+  {
+    "revision": "b0af1b83787c9da7df9c",
+    "url": "/User-Directory/static/css/main.17d40cec.chunk.css"
+  },
+  {
+    "revision": "71cb5b1c7136ae9c6562",
+    "url": "/User-Directory/static/js/2.a909d451.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/User-Directory/static/js/2.a909d451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0af1b83787c9da7df9c",
+    "url": "/User-Directory/static/js/main.4bf7ed66.chunk.js"
+  },
+  {
+    "revision": "d470e0d505db2f784f56",
+    "url": "/User-Directory/static/js/runtime-main.e6ea9e2d.js"
+  }
+]);
